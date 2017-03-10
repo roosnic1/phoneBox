0b399221a4944ba2cabe70dc3220c6d6527ac676
Nicolas Roos <nicolas.k.roos@gmail.com>
2017-03-10 19:27:13 +0100

    added a python script which uses the pygame lib to start an audio file

---
+++ POC/testAudio.py
@@ -0,0 +1,9 @@
+import pygame
+file = '123.mp3'
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load(file)
+pygame.mixer.music.play()
+
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
