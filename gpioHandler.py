d9ed2c9a04b00518f75027c44006a8dd8276af51
gustavatmel <lorenz.roos@gmail.com>
2017-03-28 14:50:56 +0200

    wheelCallback function changed for testing

--- gpioHandler.py
+++ gpioHandler.py
@@ -15,8 +15,9 @@ class GpioHandler(object):
         #self.numberDisplay = array('I', [0, 0, 0, 0])
         self.numberIter = 0
         GPIO.add_event_detect(23, GPIO.FALLING, callback=self.numberPassesCallback, bouncetime=80)
-        GPIO.add_event_detect(24, GPIO.RISING, callback=self.wheelStartedCallback, bouncetime=20)
-        GPIO.add_event_detect(24, GPIO.FALLING, callback=self.wheelFinishedCallback, bouncetime=20)
+        GPIO.add_event_detect(24, GPIO.BOTH, callback=self.wheelCallback, bouncetime=20)
+        #GPIO.add_event_detect(24, GPIO.RISING, callback=self.wheelStartedCallback, bouncetime=20)
+        #GPIO.add_event_detect(24, GPIO.FALLING, callback=self.wheelFinishedCallback, bouncetime=20)
 
         # Create display instance on default I2C address (0x70) and bus number.
         self.display = SevenSegment.SevenSegment()
@@ -30,27 +31,49 @@ class GpioHandler(object):
             self.numberDisplay[self.numberIter] = 0
         self.displayRefresher()
 
-    def wheelStartedCallback(self, Channel):
-        if self.numberIter == 0:
-            self.numberDisplay = array('I', [0, 0, 0, 0])
-        self.displayRefresher()
-
-
-    def wheelFinishedCallback(self, channel):
-        self.numberIter += 1
-        if self.numberIter >= 4:
-            discID = ''.join(str(x) for x in self.numberDisplay[:2])
-            songID = ''.join(str(x) for x in self.numberDisplay[2:])
-            currentSong = self.numberCallback(discID, songID)
-            if not currentSong[0]:
-                for i,val in enumerate(self.numberDisplay):
-                    self.numberDisplay[i] = '-'
+    def wheelCallback(self, Channel):
+        time.sleep(0.02)
+        if (GPIO.input(24) == 0):
+            self.numberIter += 1
+            if self.numberIter >= 4:
+                discID = ''.join(str(x) for x in self.numberDisplay[:2])
+                songID = ''.join(str(x) for x in self.numberDisplay[2:])
+                currentSong = self.numberCallback(discID, songID)
+                if not currentSong[0]:
+                    for i,val in enumerate(self.numberDisplay):
+                        self.numberDisplay[i] = '-'
+                    self.displayRefresher()
+                for i, val in enumerate(currentSong[1]):
+                    self.numberDisplay[i] = val
+                time.sleep(1)
                 self.displayRefresher()
-            for i, val in enumerate(currentSong[1]):
-                self.numberDisplay[i] = val
-            time.sleep(1)
+                self.numberIter = 0
+        else:
+            if self.numberIter == 0:
+                self.numberDisplay = array('I', [0, 0, 0, 0])
             self.displayRefresher()
-            self.numberIter = 0
+
+    # def wheelStartedCallback(self, Channel):
+    #     if self.numberIter == 0:
+    #         self.numberDisplay = array('I', [0, 0, 0, 0])
+    #     self.displayRefresher()
+    #
+    #
+    # def wheelFinishedCallback(self, channel):
+    #     self.numberIter += 1
+    #     if self.numberIter >= 4:
+    #         discID = ''.join(str(x) for x in self.numberDisplay[:2])
+    #         songID = ''.join(str(x) for x in self.numberDisplay[2:])
+    #         currentSong = self.numberCallback(discID, songID)
+    #         if not currentSong[0]:
+    #             for i,val in enumerate(self.numberDisplay):
+    #                 self.numberDisplay[i] = '-'
+    #             self.displayRefresher()
+    #         for i, val in enumerate(currentSong[1]):
+    #             self.numberDisplay[i] = val
+    #         time.sleep(1)
+    #         self.displayRefresher()
+    #         self.numberIter = 0
 
 
     def displayRefresher(self):
