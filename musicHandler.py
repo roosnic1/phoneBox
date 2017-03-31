5b8127ac6bd1c782e33c94cbd4d9770895202d03
Nicolas Roos <nicolas.k.roos@gmail.com>
2017-03-31 10:51:41 +0200

    changed musicHandler to work with OMXPLayer

--- musicHandler.py
+++ musicHandler.py
@@ -1,19 +1,5 @@
-import musicplayer
+from pyomxplayer import OMXPlayer
 import os.path
-
-class Song:
-    def __init__(self, fn):
-        self.url = fn
-        self.f = open(fn)
-    # `__eq__` is used for the peek stream management
-    def __eq__(self, other):
-        return self.url == other.url
-    # this is used by the player as the data interface
-    def readPacket(self, bufSize):
-        return self.f.read(bufSize)
-    def seekRaw(self, offset, whence):
-        r = self.f.seek(offset, whence)
-        return self.f.tell()
 
 
 class MusicHandler(object):
@@ -22,25 +8,21 @@ class MusicHandler(object):
         self.musicQueue = []
         self.musicDir = musicDir
         # Create our Music Player.
-        self.player = musicplayer.createPlayer()
-        self.player.outSamplerate = 96000  # support high quality :)
-        self.player.queue = self.songs()
-        self.queuePosition = 0
+        #self.player = musicplayer.createPlayer()
+        #self.player.outSamplerate = 96000  # support high quality :)
+        #self.player.queue = self.songs()
+        self.currentSong = None
         self.setDisplayCallback = setDisplayCallback
 
     def play(self, disc, track):
         songfile = self.musicDir + '/' + disc + '/' + track + '.mp3'
         if not os.path.isfile(songfile):
-            return self.musicQueue[self.queuePosition][1]
+            return self.musicQueue[0][1]
         self.musicQueue.append((songfile, disc + track))
-        self.player.playing = True
-        return self.musicQueue[self.queuePosition][1]
+        if self.currentSong is None:
+            self.currentSong = OMXPlayer(self.musicQueue[0][0], self.nextSong, start_playback=True)
+        return self.musicQueue[0][1]
 
-    def songs(self):
-        while True:
-            currentsong = self.musicQueue[self.queuePosition]
-            self.setDisplayCallback(currentsong[1])
-            yield Song(currentsong[0])
-            self.queuePosition += 1
-            if self.queuePosition >= len(self.musicQueue):
-                self.queuePosition = 0
+    def nextSong(self):
+        self.musicQueue.pop(0)
+        self.currentSong = OMXPlayer(self.musicQueue[0][0], self.nextSong, start_playback=True)
