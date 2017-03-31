6558518a481c3ac849c05d368ab97885f51721d3
gustavatmel <lorenz.roos@gmail.com>
2017-03-31 20:22:21 +0200

    gpioHandler updatd for testing

--- gpioHandler.py
+++ gpioHandler.py
@@ -60,7 +60,10 @@ class GpioHandler(object):
             if self.numberIter >= 4:
                 discID = ''.join(str(x) for x in self.numberDisplay[:2])
                 songID = ''.join(str(x) for x in self.numberDisplay[2:])
-                self.numberDisplay = self.numberCallback(discID, songID)
+                currentSong = map(int,str(self.numberCallback(discID, songID)))
+                for i, val in enumerate(currentSong):
+                    self.numberDisplay[i] = val
+
                 # currentSong = self.numberCallback(discID, songID)
                 # if not currentSong[0]:
                 #     for i,val in enumerate(self.numberDisplay):
