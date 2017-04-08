a934499bd6b282003c0113e9a7a496f50e0d75be
Nicolas Roos <nicolas.k.roos@gmail.com>
2017-04-08 15:46:05 +0200

    Also return bool if song exists or not

--- musicHandler.py
+++ musicHandler.py
@@ -7,22 +7,17 @@ class MusicHandler(object):
     def __init__(self, musicDir, setDisplayCallback):
         self.musicQueue = []
         self.musicDir = musicDir
-        # Create our Music Player.
-        #self.player = musicplayer.createPlayer()
-        #self.player.outSamplerate = 96000  # support high quality :)
-        #self.player.queue = self.songs()
         self.currentSong = None
         self.setDisplayCallback = setDisplayCallback
 
     def play(self, disc, track):
         songfile = self.musicDir + '/' + disc + '/' + track + '.mp3'
-        print('Songfile', songfile)
         if not os.path.isfile(songfile):
-            return self.musicQueue[0][1]
+            return False, self.musicQueue[0][1]
         self.musicQueue.append((songfile, "" + disc + track))
         if self.currentSong is None:
             self.currentSong = OMXPlayer(self.musicQueue[0][0], self.nextSong, start_playback=True)
-        return self.musicQueue[0][1]
+        return True, self.musicQueue[0][1]
 
     def nextSong(self):
         self.musicQueue.pop(0)
