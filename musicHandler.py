0aa2235646af397af5fdc1b23f2227efbe621377
Nicolas Roos <nicolas.k.roos@gmail.com>
2017-04-08 16:24:14 +0200

    musichandler plays endless

--- musicHandler.py
+++ musicHandler.py
@@ -1,6 +1,25 @@
 from pyomxplayer import OMXPlayer
 import os.path
 
+class song(object):
+    def __init__(self, filePath, disc, track):
+        self.filePath = filePath
+        self.disc = disc
+        self.track = track
+
+    def getDiscTrack(self):
+        return self.disc, self.track
+
+    def getFile(self):
+        return self.filePath
+
+    def getString(self):
+        return '' +  self.disc + self.track
+
+    def __str__(self):
+        return '' +  self.disc + self.track
+
+
 
 class MusicHandler(object):
 
@@ -13,15 +32,19 @@ class MusicHandler(object):
     def play(self, disc, track):
         songfile = self.musicDir + '/' + disc + '/' + track + '.mp3'
         if not os.path.isfile(songfile):
-            return False, self.musicQueue[0][1]
-        self.musicQueue.append((songfile, "" + disc + track))
+            return False, self.musicQueue[0].getString()
+        self.musicQueue.append(song(songfile, disc, track))
         if self.currentSong is None:
-            self.currentSong = OMXPlayer(self.musicQueue[0][0], self.nextSong, start_playback=True)
-        return True, self.musicQueue[0][1]
+            self.currentSong = OMXPlayer(self.musicQueue[0].getFile(), self.nextSong, start_playback=True)
+        return True, self.musicQueue[0].getString()
 
     def nextSong(self):
-        if len(self.musicQueue) > 0:
-            self.musicQueue.pop(0)
-            OMXPlayer(self.musicQueue[0][0], self.nextSong, start_playback=True)
-        else:
-            print('No more Songs to play')
+        if len(self.musicQueue) == 1:
+            tmp = self.musicQueue[0].getDiscTrack()
+            while not self.play(tmp[0],tmp[1] + 1)[0]:
+                if tmp[0] >= 99:
+                    tmp = 0,0
+                else:
+                    tmp = tmp[0] + 1, 0
+        self.musicQueue.pop(0)
+        OMXPlayer(self.musicQueue[0].getFile(), self.nextSong, start_playback=True)
