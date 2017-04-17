e448dfb9ad86d5f04dcaf0c9751148e2fa509d40
Nicolas Roos <nicolas.k.roos@gmail.com>
2017-04-17 13:20:13 +0200

    little clean up

--- musicHandler.py
+++ musicHandler.py
@@ -26,26 +26,27 @@ class song(object):
         #return '' +  self.disc + self.track
 
 
-
 class MusicHandler(object):
 
-    def __init__(self, musicDir, setDisplayCallback):
+    def __init__(self, music_dir, set_display_callback):
         self.musicQueue = []
-        self.musicDir = musicDir
+        self.musicDir = music_dir
         self.currentSong = None
-        self.setDisplayCallback = setDisplayCallback
+        self.setDisplayCallback = set_display_callback
+
+    def file_exists(self, disc, track):
+        return os.path.isfile(self.musicDir + '/' + str(disc) + '/' + str(track) + '.mp3')
 
     def play(self, disc, track):
-        newSong = song(self.musicDir, disc, track)
-        if not os.path.isfile(newSong.getFile()):
-            return False, self.musicQueue[0].getString()
-        self.musicQueue.append(newSong)
+        if not self.file_exists(disc, track):
+            return False
+        self.musicQueue.append(song(self.musicDir, disc, track))
         if self.currentSong is None:
-            self.currentSong = OMXPlayer(self.musicQueue[0].getFile(), self.nextSong, start_playback=True)
+            self.currentSong = OMXPlayer(self.musicQueue[0].getFile(), self.next_song, start_playback=True)
             self.setDisplayCallback(self.musicQueue[0].getString())
-        return True, self.musicQueue[0].getString()
+        return True
 
-    def nextSong(self):
+    def next_song(self):
         if len(self.musicQueue) == 1:
             tmp = self.musicQueue[0].getDiscTrack()
             while not self.play(tmp[0], tmp[1] + 1)[0]:
@@ -55,7 +56,7 @@ class MusicHandler(object):
                 else:
                     tmp = tmp[0] + 1, -1
         self.musicQueue.pop(0)
-        self.currentSong = OMXPlayer(self.musicQueue[0].getFile(), self.nextSong, start_playback=True)
+        self.currentSong = OMXPlayer(self.musicQueue[0].getFile(), self.next_song, start_playback=True)
         self.setDisplayCallback(self.musicQueue[0].getString())
 
     def stop(self):
