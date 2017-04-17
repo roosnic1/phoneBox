947261fa42bbe27ca14916e41af20605154345d4
Nicolas Roos <nicolas.k.roos@gmail.com>
2017-04-17 12:27:47 +0200

    added pass

--- gpioHandler.py
+++ gpioHandler.py
@@ -85,6 +85,7 @@ class GpioHandler(object):
                 self.numberDisplay = ['','','','']
                 self.lookFlag = False
             else:
+                pass
                 #self.t.start()
 
 
